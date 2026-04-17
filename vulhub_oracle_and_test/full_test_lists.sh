acb392f3d05185cc460029fbbac458b23f8f0849
oldjeffspectator <oldjeffpsectator@gmail.com>
2026-04-17 13:45:44 +0800

    add case vulhub/apache-druid/CVE-2021-25646

--- vulhub_oracle_and_test/full_test_lists.sh
+++ vulhub_oracle_and_test/full_test_lists.sh
@@ -1,2 +1,2 @@
 bash ./run_oracle_and_test.sh /data1/jph/VulRL/benchmark/vulhub/aj-report/CNVD-2024-15077
-
+bash ./run_oracle_and_test.sh /data1/jph/VulRL/benchmark/vulhub/apache-druid/CVE-2021-25646
