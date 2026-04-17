787dc506bbfeafc9d5f44f1fe1e5cc532d63a1de
oldjeffspectator <oldjeffpsectator@gmail.com>
2026-04-17 18:56:08 +0800

    add oracle to E:\git_fork_folder\VulRL\benchmark\vulhub\activemq\CVE-2016-3088 and E:\git_fork_folder\VulRL\benchmark\vulhub\airflow\CVE-2020-11981

--- vulhub_oracle_and_test/full_test_lists.sh
+++ vulhub_oracle_and_test/full_test_lists.sh
@@ -2,3 +2,5 @@ bash ./run_oracle_and_test.sh /data1/jph/VulRL/benchmark/vulhub/aj-report/CNVD-2
 bash ./run_oracle_and_test.sh /data1/jph/VulRL/benchmark/vulhub/apache-druid/CVE-2021-25646
 bash ./run_oracle_and_test.sh /data1/jph/VulRL/benchmark/vulhub/apisix/CVE-2020-13945
 bash ./run_oracle_and_test.sh /data1/jph/VulRL/benchmark/vulhub/apisix/CVE-2021-45232
+bash ./run_oracle_and_test.sh /data1/jph/VulRL/benchmark/vulhub/activemq/CVE-2016-3088
+bash ./run_oracle_and_test.sh /data1/jph/VulRL/benchmark/vulhub/airflow/CVE-2020-11981
